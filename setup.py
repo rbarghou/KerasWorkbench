f2e6e01b9420a97c688decea4ffcd0ab8928651d
ramseybarghouti <ramsey@addgene.org>
2018-07-25 22:20:19 -0400

    added basic setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from distutils.core import setup
 
+setup(
+    name='KerasWorkbench',
+    version='0.1dev',
+    author='Ramsey Barghouti',
+    packages=['kerasworkbench',],
+    license='GPL-3.0',
+    long_description=open('README.txt').read(),
+)
