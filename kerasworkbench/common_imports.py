00b7330214d4889ba0d5bcd28a7a71a43cd0a3df
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-07 11:17:30 -0400

    Added common import script.

---
+++ kerasworkbench/common_imports.py
@@ -0,0 +1,28 @@
+"""
+This script is intended as a simple tool for use with Google Colab platform
+
+It's intended use is as follows
+`from kerasworkbench.common_imports import *`
+
+This is not intended for use in production software and is only intended to
+simplify setup cells in colab.
+"""
+
+
+import numpy as np
+import scipy as sp
+import scipy.signal as signal
+from scipy.signal import (
+    periodogram,
+)
+
+import librosa
+from librosa import (
+    amplitude_to_db,
+    stft,
+    istft,
+)
+
+from librosa.display import (
+    specshow
+)
