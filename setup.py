4cc6b4ee09ecade88cc4e6e69436376b130802a3
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-07 10:58:23 -0400

    update to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name='KerasWorkbench',
     version='0.1dev',
     author='Ramsey Barghouti',
+    description="keras workbench for simple tooling that I would like to use across platforms",
     packages=['kerasworkbench',],
     license='GPL-3.0',
     long_description=open('README.md').read(),
+    install_requires=[
+        "keras",
+        "librosa"
+    ]
 )
