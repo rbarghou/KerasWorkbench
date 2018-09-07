23a87a731a0146ff3c4eaaa71b156d0ed1159653
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-07 13:18:27 -0400

    change name to note.

--- kerasworkbench/note.py
+++ kerasworkbench/note.py
@@ -6,7 +6,7 @@ C0 = A4 * power(2, -4.75)
 name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 
-def pitch(freq):
+def note(freq):
     h = int(round(12 * log2(freq / C0)))
     octave = h // 12
     n = h % 12
