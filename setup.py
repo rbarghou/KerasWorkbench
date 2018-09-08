e11ec98e9040b7e07ee97bb3601f26e5830195c8
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-08 08:09:16 -0400

    added packages to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version='0.1dev',
     author='Ramsey Barghouti',
     description="keras workbench for simple tooling that I would like to use across platforms",
-    packages=['kerasworkbench',],
+    packages=[
+        'kerasworkbench',
+        'kerasworkbench.data',
+        'kerasworkbench.data.audio',
+        'kerasworkbench.generators',
+    ],
     license='GPL-3.0',
     long_description=open('README.md').read(),
     install_requires=[
