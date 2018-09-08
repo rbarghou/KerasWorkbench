ff32e940c809d7b214adfe41a01cc885f7638a6c
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-08 07:26:12 -0400

    Added some basic data loading functionality.

---
+++ kerasworkbench/data/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+import librosa
+
+
+SUPPORTED_FORMATS = ("mp3")
+
+DATA_DIR = os.path.dirname(__file__)
+AUDIO_DATA_DIR = os.path.join(DATA_DIR, "audio")
+
+AUDIO_FILES = [
+    fn for fn in os.listdir(AUDIO_DATA_DIR)
+    if any(fn.endswith(format) for format in SUPPORTED_FORMATS)
+]
+
+
+def load_audio_file(i, *args, **kwargs):
+    return librosa.load(os.path.join(AUDIO_DATA_DIR, AUDIO_FILES[i]), *args, **kwargs)
