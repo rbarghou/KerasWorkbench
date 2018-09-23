1fa30a99a7bd63b7d2a4ac5723f16cab3e0e6399
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-09-23 07:28:37 -0400

    Added docstring.

--- kerasworkbench/hsp.py
+++ kerasworkbench/hsp.py
@@ -2,6 +2,17 @@ import numpy as np
 
 
 def hsp(spectrogram, num_harmonics):
+    """Harmonic Spectrum Product
+    Returns an array where every frequency bin is the product of it's harmonics up to num_harmonics
+
+    This function is useful for trying to identify the fundamental frequencies in an audio sample where
+    harmonics are present.  It will not identify the specific harmonic, but where harmonics are present
+    this function should amplify the fundamentals.  Where they are absent, the signal should be dampened.
+
+    :param spectrogram: 2d array of real or complex numbers of the shape [n_frequency_bins x n_frames]
+    :param num_harmonics: The number of harmonics to consider in the HSP analysis
+    :return result: A real array with the same shape as the spectrogram
+    """
     return np.array([
         np.product(
             np.abs(spectrogram)[::f0, :][:num_harmonics, :],
